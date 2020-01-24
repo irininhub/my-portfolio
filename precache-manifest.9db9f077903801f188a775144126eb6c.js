b3116e9a12bbd67fa1d03b3fcb298b1dd0f0aafb
Irina <irina.potrebko7@gmail.com>
2020-01-24 13:59:22 +0300

    Updates

---
+++ precache-manifest.9db9f077903801f188a775144126eb6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2627063c02aa9dcfe60927d58a076a93",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0a822eba8c4ae6db7c01",
+    "url": "/my-portfolio/static/css/main.e250b490.chunk.css"
+  },
+  {
+    "revision": "39b46a59ee9fde10388a",
+    "url": "/my-portfolio/static/js/2.d75612d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-portfolio/static/js/2.d75612d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a822eba8c4ae6db7c01",
+    "url": "/my-portfolio/static/js/main.44c37868.chunk.js"
+  },
+  {
+    "revision": "4184a7eacb7c48bd5c06",
+    "url": "/my-portfolio/static/js/runtime-main.4a96037b.js"
+  }
+]);
